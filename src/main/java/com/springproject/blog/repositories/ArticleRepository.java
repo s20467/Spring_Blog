b0d2ab9a9548deadedfea24de2081cc14d18d15a
Hubert Orzełowski <s20467@pja.edu.pl>
2021-05-14 00:47:01 +0200

    Add methods to ArticleRepository
    Added methods to:
    find article with title like %title% (case insensitive)
    find article with author's username like %user% (case insensitive)

--- src/main/java/com/springproject/blog/repositories/ArticleRepository.java
+++ src/main/java/com/springproject/blog/repositories/ArticleRepository.java
@@ -1,12 +1,21 @@
 package com.springproject.blog.repositories;
 
 import com.springproject.blog.models.Article;
+import com.springproject.blog.models.dto.ArticleDto;
 import com.springproject.blog.models.security.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
 public interface ArticleRepository extends CrudRepository<Article, Integer> {
     Iterable<Article> getArticlesByAuthor(User user);
     Iterable<Article> getArticlesByTitle(String title);
+
+    @Query("from Article a where lower(a.title) like :titleLike")
+    Iterable<Article> findArticlesByTitleLikeIgnoreCase(@Param("titleLike") String titleLike);
+
+    @Query("select a from Article a left join a.author author where lower(author.username) like :authorLike")
+    Iterable<Article> findArticlesByAuthorLikeIgnoreCase(@Param("authorLike") String authorLike);
 }
