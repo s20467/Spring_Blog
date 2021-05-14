b294968dae062b3f47ed64aa0164e571008ab2f5
Hubert Orzełowski <s20467@pja.edu.pl>
2021-05-14 13:21:59 +0200

    Add temporary redirecting IndexController

--- src/main/java/com/springproject/blog/controllers/IndexController.java
+++ src/main/java/com/springproject/blog/controllers/IndexController.java
@@ -1,2 +1,13 @@
-package com.springproject.blog.controllers;public class IndexController {
+package com.springproject.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("")
+    public String redirectToArticleHomePage(){
+        return "redirect:/article";
+    }
 }
