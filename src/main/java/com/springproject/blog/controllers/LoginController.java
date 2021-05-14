981782a63fcf9ea6f2f85b93ac8e41d7ee4f9692
Hubert Orzełowski <s20467@pja.edu.pl>
2021-05-14 13:21:23 +0200

    Add LoginController

--- src/main/java/com/springproject/blog/controllers/LoginController.java
+++ src/main/java/com/springproject/blog/controllers/LoginController.java
@@ -1,2 +1,13 @@
-package com.springproject.blog.controllers;public class LoginController {
+package com.springproject.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login_form";
+    }
 }
