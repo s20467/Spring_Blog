bec6ddf31e1f6a1f6a4c9f7b7e7c65f1a3973b6a
Hubert Orzełowski <s20467@pja.edu.pl>
2021-05-14 22:08:19 +0200

    Edit bootstrap data

--- src/main/java/com/springproject/blog/bootstrap/bootstrap.java
+++ src/main/java/com/springproject/blog/bootstrap/bootstrap.java
@@ -36,11 +36,28 @@ public class bootstrap implements CommandLineRunner {
 
 
     private void loadSecurityData() {
-        log.debug("bootstrap data");
 
-        Authority getSecuredPageAuthority = authorityRepository.save(Authority.builder().permission("secured").build());
+        Authority createArticle = authorityRepository.save(Authority.builder().permission("article.create").build());
+        Authority readArticle = authorityRepository.save(Authority.builder().permission("article.read").build());
+        Authority updateArticle = authorityRepository.save(Authority.builder().permission("article.update").build());
+        Authority deleteArticle = authorityRepository.save(Authority.builder().permission("article.update").build());
 
-        Role userRole = roleRepository.save(Role.builder().authority(getSecuredPageAuthority).name("ROLE_USER").build());
+        Authority createArticleUser = authorityRepository.save(Authority.builder().permission("user.article.create").build());
+        Authority readArticleUser = authorityRepository.save(Authority.builder().permission("user.article.read").build());
+        Authority updateArticleUser = authorityRepository.save(Authority.builder().permission("user.article.update").build());
+        Authority deleteArticleUser = authorityRepository.save(Authority.builder().permission("user.article.update").build());
+
+
+
+        Role userRole = roleRepository.save(Role.builder()
+                .authorities(Set.of(createArticleUser, readArticleUser, updateArticleUser, deleteArticleUser))
+                .name("ROLE_USER").build());
+
+        Role adminRole = roleRepository.save(Role.builder()
+                .authorities(Set.of(createArticle, readArticle, updateArticle, deleteArticle))
+                .name("ROLE_ADMIN").build());
+
+
 
         User user = User.builder()
                 .username("user")
@@ -48,7 +65,21 @@ public class bootstrap implements CommandLineRunner {
                 .role(userRole)
                 .build();
 
+        User user2 = User.builder()
+                .username("user2")
+                .password(passwordEncoder.encode("pass"))
+                .role(userRole)
+                .build();
+
+        User admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("pass"))
+                .role(adminRole)
+                .build();
+
         userRepository.save(user);
+        userRepository.save(user2);
+        userRepository.save(admin);
 
         Article article1 = Article.builder()
                 .author(user)
@@ -67,7 +98,7 @@ public class bootstrap implements CommandLineRunner {
                 .build();
 
         Article article3 = Article.builder()
-                .author(user)
+                .author(user2)
                 .title("tYtUl3")
                 .content("3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
                 .createdAt(Timestamp.valueOf(LocalDateTime.now()))
@@ -75,7 +106,7 @@ public class bootstrap implements CommandLineRunner {
                 .build();
 
         Article article4 = Article.builder()
-                .author(user)
+                .author(user2)
                 .title("TyTuL4")
                 .content("4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
                 .createdAt(Timestamp.valueOf(LocalDateTime.now()))
