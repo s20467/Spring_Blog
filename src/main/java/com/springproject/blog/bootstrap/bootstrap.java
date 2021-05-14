e28112ea6b7a2dd811a3393491b39580d3ce53a0
Hubert Orzełowski <s20467@pja.edu.pl>
2021-05-14 17:49:05 +0200

    Add more example data in bootstrap

--- src/main/java/com/springproject/blog/bootstrap/bootstrap.java
+++ src/main/java/com/springproject/blog/bootstrap/bootstrap.java
@@ -1,8 +1,10 @@
 package com.springproject.blog.bootstrap;
 
+import com.springproject.blog.models.Article;
 import com.springproject.blog.models.security.Authority;
 import com.springproject.blog.models.security.Role;
 import com.springproject.blog.models.security.User;
+import com.springproject.blog.repositories.ArticleRepository;
 import com.springproject.blog.repositories.security.AuthorityRepository;
 import com.springproject.blog.repositories.security.RoleRepository;
 import com.springproject.blog.repositories.security.UserRepository;
@@ -11,6 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -21,6 +27,7 @@ public class bootstrap implements CommandLineRunner {
     private final RoleRepository roleRepository;
     private final AuthorityRepository authorityRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ArticleRepository articleRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -42,5 +49,39 @@ public class bootstrap implements CommandLineRunner {
                 .build();
 
         userRepository.save(user);
+
+        Article article1 = Article.builder()
+                .author(user)
+                .title("TiTlE1")
+                .content("1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .lastModified(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        Article article2 = Article.builder()
+                .author(user)
+                .title("tItLe2")
+                .content("2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .lastModified(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        Article article3 = Article.builder()
+                .author(user)
+                .title("tYtUl3")
+                .content("3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .lastModified(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        Article article4 = Article.builder()
+                .author(user)
+                .title("TyTuL4")
+                .content("4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .lastModified(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        articleRepository.saveAll(Set.of(article1, article2, article3, article4));
     }
 }
